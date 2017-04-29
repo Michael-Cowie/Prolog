be7bdb22852bae5f3644e74dcc398070a28241d5
Michael Cowie <mco116@uclive.ac.nz>
2017-04-29 16:30:29 +1200

    Update doesNotContainX.pl

--- doesNotContainX.pl
+++ doesNotContainX.pl
@@ -3,9 +3,9 @@ doesNotContainX/3 succeeds if the list given in the third parameter matches the 
 parameter if all instances of X given as the first parameter is removed.
 */
 
-remove(X, [], []).
-remove(X, [X | Tail], ListOut ) :- remove(X, Tail, ListOut).
-remove(X, [Y | Tail1], [Y | Tail2]) :- X \= Y, remove(X, Tail1, Tail2).
+doesNotContainX(X, [], []).
+doesNotContainX(X, [X | Tail], ListOut ) :- doesNotContainX(X, Tail, ListOut).
+doesNotContainX(X, [Y | Tail1], [Y | Tail2]) :- X \= Y, doesNotContainX(X, Tail1, Tail2).
 
 /*
 Example query.
