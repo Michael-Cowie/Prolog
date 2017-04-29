135853b6b64f9833f9674143cc9af68a83b677e1
Michael Cowie <mco116@uclive.ac.nz>
2017-04-29 15:40:37 +1200

    Update fibonacci.pl

--- fibonacci.pl
+++ fibonacci.pl
@@ -1,3 +1,8 @@
+/*
+fib/2 is a predicate that takes the first parameter as the nth fibonacci
+sequence and the 2nd parameter as the solution.
+*/
+
 fib(N, [0, 1 | Tail]) :- fibrel(N, [0, 1 | Tail]).
 
 fibrel(2,[X,Y]).
@@ -6,5 +11,13 @@ fibrel(N, [Front, Next, Sum | Tail]) :-
       Sum is Next + Front,
       fibrel(NewN, [Next, Sum | Tail]).
       
+/*
+Example query:
+
+example_query :- fib(6, X),
+               writeln(X).
+Output:
+[0,1,1,2,3,5]
+*/
       
       
